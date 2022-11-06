c99ae68ce473027c7bf64c13cc0439d363775eee
thdtjdgml415 <thdtjdgml415@naver.com>
2022-11-06 15:36:48 +0900

    2022-11-06

--- src/components/include/MovieCont.jsx
+++ src/components/include/MovieCont.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 
 function MovieCont({ movies }) {
-  console.log(movies);
   return (
     <div className="movie__wrap">
       <div className="container">
+        <h1>MOVIE</h1>
         <div className="movie__inner">
-          <div className="movie__box">
-            {
-              // movies.map((e)=>(
-              // ))
-            }
-          </div>
+          <div className="movie__box">{}</div>
         </div>
       </div>
     </div>
