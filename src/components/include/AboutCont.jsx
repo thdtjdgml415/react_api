f07b97bd50b5eae7df15c072f2234517ddd92b21
thdtjdgml415 <thdtjdgml415@naver.com>
2022-11-15 17:33:11 +0900

    2022-11-15

--- src/components/include/AboutCont.jsx
+++ src/components/include/AboutCont.jsx
@@ -5,9 +5,20 @@ function AboutCont() {
     <section className="cont__about">
       <div className="container">
         <img
-          src="/src/assets/img/vincenzo-malagoli-flfhAlEwDq4-unsplash.jpg"
+          src="https://github.com/thdtjdgml415/codingclass/blob/main/javascript/assets/img/effect_bg_16.jpg?raw=true"
           alt=""
         />
+        <p className="about__desc">
+          북극성이 가장 밝은 별이 아니라는 것을 알고 있으신가요?
+          <br />
+          <br />
+          사람들은 북극성의 찬란한 반짝임을 통해 바닷길을 찾을 수 있다고
+          생각하는데요? 하지만 저는 북극성이 아닌 다른 별자리의 도움을 받고
+          바람과 물결 등 자연의 조화에 도움을 받았다고 생각합니다. <br /> 홀로
+          전진하기보다 조화와 공존의 힘으로 올바른 길을 찾을 수 있었다고
+          생각합니다. 저는 조화와 공존이라는 것에 중요한 가치를 두는 개발자가
+          되겠습니다.
+        </p>
       </div>
     </section>
   );
