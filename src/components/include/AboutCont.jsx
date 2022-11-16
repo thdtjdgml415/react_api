02f000a804a2be7fa6e5516ef231b5712eb3d513
thdtjdgml415 <thdtjdgml415@naver.com>
2022-11-16 09:15:20 +0900

    2022-11-16

--- src/components/include/AboutCont.jsx
+++ src/components/include/AboutCont.jsx
@@ -19,6 +19,21 @@ function AboutCont() {
           생각합니다. 저는 조화와 공존이라는 것에 중요한 가치를 두는 개발자가
           되겠습니다.
         </p>
+        <div className="about__desc2">
+          <img
+            src="https://github.com/thdtjdgml415/react_api/blob/master/src/assets/img/josh-miller-vWBKt9Dto1g-unsplash.jpg?raw=true"
+            alt=""
+          />
+          <p className="about__inner__desc">
+            사람들은 북극성의 찬란한 반짝임을 통해 바닷길을 찾을 수 있다고
+            생각하는데요? 하지만 저는 북극성이 아닌 다른 별자리의 도움을 받고
+            바람과 물결 등 자연의 조화에 도움을 받았다고 생각합니다. <br /> 홀로
+            전진하기보다 조화와 공존의 힘으로 올바른 길을 찾을 수 있었다고
+            생각합니다. 저는 조화와 공존이라는 것에 중요한 가치를 두는 개발자가
+            되겠습니다.
+          </p>
+        </div>
+        <div className="about__desc3"></div>
       </div>
     </section>
   );
